87a67149c320f366d3735b7f01833ff206cfda58
srinadh-code <yalagandulasrinadh@gmail.com>
2026-02-03 10:46:34 +0530

    added_authen

--- Userinfo/urls.py
+++ Userinfo/urls.py
@@ -1,19 +1,9 @@
-# from django.urls import path
-# from .views import ProfileView,profile_page
-# urlpatterns=[
-#     path('profile/',ProfileView.as_view()),
-#     path("api/profile/", ProfileView.as_view(), name="api_profile"),
-#     path("profile/", profile_page, name="profile_page"),
-    
-# ]
-
 from django.urls import path
-from .views import ProfileView, profile_page
+from .views import ProfileView,profile_page
+urlpatterns=[
+    path('profile/',ProfileView.as_view()),
 
-urlpatterns = [
-    #  HTML Profile Page
-    path("profile/", profile_page, name="profile_page"),
-
-    # DRF API Profile
-    path("api", ProfileView.as_view(), name="api_profile"),
+    
 ]
+
+
